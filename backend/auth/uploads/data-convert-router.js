b480b6fb3b54ffb5ddc770d406b08e106e18bd94
salem yahya maetalla <ip19293.etu@iscae.mr>
2024-02-11 14:36:51 +0100

    change all conception1

--- backend/auth/uploads/data-convert-router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const xlsxController = require("./xlsx");
-//const authController = require("../controller/auth-controller");
-const xlsxRouter = express.Router();
-xlsxRouter.route("/").get(xlsxController.xlsx_to_json);
-module.exports = xlsxRouter;
